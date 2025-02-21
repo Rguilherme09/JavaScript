e07e7a50d3dda89c49f7a2eab50d8ff0e9118c2d
Rodrigo Guilherme <rguilherme419@hotmail.com>
2025-02-21 16:25:59 -0300

    updating files

---
+++ jogo-da-forca/dados/dados.js
@@ -0,0 +1,5 @@
+const listaDeFrutas = [
+    'Pera', 'Banana', 'Laranja', 'Abacaxi', 'Manga', 'Uva', 'Abacate', 'Jaca', 'Caju', 'Melancia', 'Carambola', 'Kiwi', 'Goiaba', 'Cereja', 'Morango', 'Maracuja', 'Pêssego', 'Pequi', 'Pitanga', 'Tangerina', 'Tamarindo', 'Caju', 'Manga', 'Jambo', 'Jabuticaba', 'Groselha', 'Figo', 'Framboesa', 'Caqui', 'Cacau'
+];
+
+export default listaDeFrutas;
